c33024817cb08afd87495cc5ceec748095f6a8ed
Vincent <vincentallan001@gmail.com>
2023-06-15 05:46:24 -0700

    Added some functions and fixed the menu bug

--- CarImpl.java
+++ CarImpl.java
@@ -1,3 +1,181 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class CarImpl implements CarInter{
-Main main = new Main();
+    Scanner sc = new Scanner(System.in);
+//Main main = new Main();
+
+public void check(String[]sub, String x){
+    boolean test
+            = Arrays.asList(sub)
+            .contains(x);
+    if (test){
+       System.out.printf("The vehicle %s is available", x);
+       if (x.equals( "Impreza")){
+           System.out.println("""
+                   1.Maximum power\t100 - 280ps
+                   2.Fuel Consumption\t10 - 15km/L
+                   3.Drive Type\tAWD/FF
+                   4.Engine Capacity\t1,493 - 1,995cc
+                   5.Number of Seats\t5
+                   6.Rental price\tKsh 4000 per day""");
+       } else if (x.equals("Forester")) {
+           System.out.println("""
+                   1.Maximum power\t230 - 233.1ps
+                   2.Fuel Consumption\t15 - 20km/L
+                   3.Drive Type\tAWD/FF
+                   4.Engine Capacity\t1,493 - 1,995cc
+                   5.Number of Seats\t5
+                   6.Rental price\tKsh 4500 per day""");
+       }
+   }
+
+}
+    public void check2(String[]bmw, String y){
+        boolean test
+                = Arrays.asList(bmw)
+                .contains(y);
+        if (test){
+            System.out.printf("The vehicle %s is available", y);
+            if (y.equals( "Impreza")){
+                System.out.println("""
+                        1.Maximum power\t100 - 280ps
+                        2.Fuel Consumption\t10 - 15km/L
+                        3.Drive Type\tAWD/FF
+                        4.Engine Capacity\t1,493 - 1,995cc
+                        5.Number of Seats\t5
+                        6.Rental price\tKsh 4000 per day""");
+            } else if (y.equals("Forester")) {
+                System.out.println("""
+                        1.Maximum power\t230 - 233.1ps
+                        2.Fuel Consumption\t15 - 20km/L
+                        3.Drive Type\tAWD/FF
+                        4.Engine Capacity\t1,493 - 1,995cc
+                        5.Number of Seats\t5
+                        6.Rental price\tKsh 4500 per day""");
+            }
+        }
+
+    }
+    public void check3(String[]sub, String x){
+        boolean test
+                = Arrays.asList(sub)
+                .contains(x);
+        if (test){
+            System.out.printf("The vehicle %s is available", x);
+            if (x.equals( "Impreza")){
+                System.out.println("""
+                        1.Maximum power\t100 - 280ps
+                        2.Fuel Consumption\t10 - 15km/L
+                        3.Drive Type\tAWD/FF
+                        4.Engine Capacity\t1,493 - 1,995cc
+                        5.Number of Seats\t5
+                        6.Rental price\tKsh 4000 per day""");
+            } else if (x.equals("Forester")) {
+                System.out.println("""
+                        1.Maximum power\t230 - 233.1ps
+                        2.Fuel Consumption\t15 - 20km/L
+                        3.Drive Type\tAWD/FF
+                        4.Engine Capacity\t1,493 - 1,995cc
+                        5.Number of Seats\t5
+                        6.Rental price\tKsh 4500 per day""");
+            }
+        }
+
+    }
+    public void check4(String[]toyota, String t){
+        boolean test
+                = Arrays.asList(toyota)
+                .contains(t);
+        if (test){
+            System.out.printf("The vehicle %s is available", t);
+            if (t.equals( "Impreza")){
+                System.out.println("""
+                        1.Maximum power\t100 - 280ps
+                        2.Fuel Consumption\t10 - 15km/L
+                        3.Drive Type\tAWD/FF
+                        4.Engine Capacity\t1,493 - 1,995cc
+                        5.Number of Seats\t5
+                        6.Rental price\tKsh 4000 per day""");
+            } else if (t.equals("Forester")) {
+                System.out.println("""
+                        1.Maximum power\t230 - 233.1ps
+                        2.Fuel Consumption\t15 - 20km/L
+                        3.Drive Type\tAWD/FF
+                        4.Engine Capacity\t1,493 - 1,995cc
+                        5.Number of Seats\t5
+                        6.Rental price\tKsh 4500 per day""");
+            }
+        }
+
+    }
+    public void check5(String[]nissan, String n){
+        boolean test
+                = Arrays.asList(nissan)
+                .contains(n);
+        if (test){
+            System.out.printf("The vehicle %s is available", n);
+            if (n.equals( "Impreza")){
+                System.out.println("""
+                        1.Maximum power\t100 - 280ps
+                        2.Fuel Consumption\t10 - 15km/L
+                        3.Drive Type\tAWD/FF
+                        4.Engine Capacity\t1,493 - 1,995cc
+                        5.Number of Seats\t5
+                        6.Rental price\tKsh 4000 per day""");
+            } else if (n.equals("Forester")) {
+                System.out.println("""
+                        1.Maximum power\t230 - 233.1ps
+                        2.Fuel Consumption\t15 - 20km/L
+                        3.Drive Type\tAWD/FF
+                        4.Engine Capacity\t1,493 - 1,995cc
+                        5.Number of Seats\t5
+                        6.Rental price\tKsh 4500 per day""");
+            }
+        }
+
+    }
+    public void choice(){
+        System.out.println("Enter choice: ");
+        int choices = sc.nextInt();
+
+        switch (choices) {
+            case 1 -> {
+                System.out.print("Enter the subaru model:  ");
+                String x = sc.next();
+                String[] sub = {"Impreza", "Forester", "Crosstrek", "Ascent", "Outback", "Legacy", "WRX"};
+                check(sub, x);
+            }
+            case 2 -> {
+                System.out.print("Enter the BMW model: ");
+                String y = sc.next();
+                String[] bmw = {"X-series", "2-series", "3-series"};
+                check2(bmw, y);
+            }
+            case 3 -> {
+                System.out.print("Enter the mercedes model: ");
+                String z = sc.next();
+                String[] merce = {"C200", "E250", "S550", "GLE"};
+                check3(merce, z);
+            }
+            case 4 -> {
+                System.out.print("Enter the Range rover model: ");
+                String r = sc.next();
+                String[] range = {"Sport", "Vogue", "Evoque"};
+                check4(range, r);
+            }
+            case 5 -> {
+                System.out.print("Enter the toyota model: ");
+                String t = sc.next();
+                String[] toyota = {"Sedans", "SUV", "Sport"};
+                check5(toyota, t);
+            }
+            case 6 -> {
+                System.out.print("Enter the nissan model: ");
+                String n = sc.next();
+                String[] nissan = {"Sedans", "SUV", "Sport"};
+                check5(nissan, n);
+            }
+        }
+    }
 }
--- Main.java
+++ Main.java
@@ -1,28 +1,21 @@
-import java.util.Objects;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
-public class Main {
-    Scanner sc = new Scanner(System.in);
+public class  Main {
     public void menu(){
         System.out.println("login Successful!!");
+
         System.out.println("""
-                                1.Subaru\s
-                                 2.Volvo\s
+                                 1.Subaru\s
+                                 2.BMW\s
                                  3.Mercedes\s
                                  4.Range Rover\s
                                  5.Toyota\s
                                  6.Nissan\s
                                  7.Exit""");
     }
-    public void choice(){
-        System.out.println("Enter choice: ");
-        int choices = sc.nextInt();
 
-        switch (choices){
-            case 1:
-                
-        }
-    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -33,30 +26,55 @@ public class Main {
         String pass = "a1man";
         String user = "vince";
 
+        //JUST VINCE DON'T MIND :)
+
+        System.out.println(" \\**                **/");
+        System.out.println("  \\**              **/");
+        System.out.println("   \\**            **/");
+        System.out.println("    \\**          **/");
+        System.out.println("     \\**        **/");
+        System.out.println("      \\**      **/");
+        System.out.println("       \\**    **/");
+        System.out.println("        \\$$$$$$$");
+
         System.out.println("Welcome to Vince Car Rental place :)");
         System.out.println("1.Sign up\n2.Login");
         int ans = sc.nextInt();
 
         if (ans == 1) {
             System.out.println("Welcome to Vince Car rental");
-            System.out.println("Please enter your username: ");
+            System.out.print("Please enter your username: ");
             String userName = sc.next();
 
-            System.out.println("Please set your password: ");
+            Map<String, String> map = new HashMap<>();
+            map.put(userName, userName);
+
+            System.out.print("Please set your password: ");
             String newPass = sc.next();
-            System.out.println("Confirm your password: ");
+            System.out.print("Confirm your password: ");
             String confirmPass = sc.next();
 
             if (confirmPass.equals(newPass)){
                 System.out.println("login Successful!!");
                 while (true){
-                    main.menu();
+                    //try-catch just for the fun of it
+                    try{
+                        main.menu();
+                        impl.choice();
+                        break;
+                    }
+                    catch (Exception e){
+                        System.out.println("Vince");
+                    }
+                    finally {
+                        System.out.println("**/*/*/*/*//*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/");
+                    }
                 }
             }
             else {
                 int count = 2;
                 while (count > 0 ){
-                    System.out.println("Wrong confirmation!! Try again: ");
+                    System.out.print("Wrong confirmation!! Try again: ");
                     confirmPass = sc.next();
                     count -= 1;
                 }
@@ -64,16 +82,16 @@ public class Main {
 
 
         } else if (ans == 2) {
-            System.out.println("Enter your username: ");
+            System.out.print("Enter your username: ");
             car.setUserName(sc.next());
 
-            System.out.println("Enter your password: ");
+            System.out.print("Enter your password: ");
             car.setPassword(sc.next());
 
                 if ((user.equals(car.getUserName()) & (pass.equals(car.getPassword())))){
-                        while (true){
                             main.menu();
-                        }
+                            impl.choice();
+
             }
                 else {
                     System.exit(0);
